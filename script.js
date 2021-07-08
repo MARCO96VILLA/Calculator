b0512d024ad9135d86af5eff6a5ad28ba04110c1
MARCO96VILLA <marco@DESKTOP-PFVCIFV.(none)>
2021-07-08 18:27:25 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let display = document.querySelector("#display");
+let focus = () => document.querySelector("#display").focus();
+let clear = document.querySelector("#clear");
+let select = (num) => document.querySelector("#" + num);
+let firstNum = "";
+let operator = "";
+display.value = "";
+focus();
+
 let add = (a, b) => Number(a) + Number(b);
 
 let subtract = (a, b) => a - b;
@@ -23,153 +32,132 @@ let operate = (operator, a, b) => {
         return pow(a, b);
     }
 };
-
-let display = document.querySelector("#display");
-let clear = document.querySelector("#clear");
-let select = (num) => document.querySelector("#" + num);
-let firstNum = "";
-let operator = "";
-display.value = "";
-
+let memorize = () => {
+    if (firstNum !== "") {
+        let res = "";
+        if (operator == "+") {
+            res = add(firstNum, display.value);
+        } else if (operator == "-") {
+            res = subtract(firstNum, display.value);
+        } else if (operator == "*") {
+            res = multiply(firstNum, display.value);
+        } else if (operator == "/") {
+            res = divide(firstNum, display.value);
+        } else if (operator == "**") {
+            res = pow(firstNum, display.value);
+        }
+        firstNum = res;
+    } else {
+        firstNum = display.value;
+    }
+};
+let resetFocus = (op) => {
+    display.value = "";
+    operator = op;
+    focus();
+};
+let addq = () => {
+    memorize();
+    resetFocus("+");
+};
+let subtq = () => {
+    memorize();
+    resetFocus("-");
+};
+let multq = () => {
+    memorize();
+    resetFocus("*");
+};
+let divideq = () => {
+    memorize();
+    resetFocus("/");
+};
+let powq = () => {
+    memorize();
+    resetFocus("**");
+};
+let equalq = () => {
+    secondNum = display.value;
+    display.value = operate(operator, firstNum, secondNum);
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    focus();
+};
+let clearq = () => {
+    firstNum = "";
+    operator = "";
+    secondNum = "";
+    display.value = "";
+    focus();
+};
+let squareq = () => {
+    firstNum = display.value
+    display.value = sqrt(firstNum);
+    firstNum = "";
+    operator = "";
+    focus();
+};
+let delq = () => {
+    display.value = display.value.slice(0, -1);
+    focus();
+};
+let pointq = () => {
+    if (!display.value.includes(".")) {
+        display.value = display.value + ".";
+    }
+    focus();
+};
 document.querySelectorAll(".number-input").forEach(el => {
     el.addEventListener("click", () => {
         display.value += el.value
     })
 });
 
-document.querySelector("#add").addEventListener("click", () => {
-    if (firstNum !== "") {
-        let res = "";
-        if (operator == "+") {
-            res = add(firstNum, display.value);
-        } else if (operator == "-") {
-            res = subtract(firstNum, display.value);
-        } else if (operator == "*") {
-            res = multiply(firstNum, display.value);
-        } else if (operator == "/") {
-            res = divide(firstNum, display.value);
-        } else if (operator == "**") {
-            res = pow(firstNum, display.value);
-        }
-        firstNum = res;
+document.querySelector("#add").addEventListener("click", addq);
+document.querySelector("#subt").addEventListener("click", subtq);
+document.querySelector("#mult").addEventListener("click", multq);
+document.querySelector("#divide").addEventListener("click", divideq);
+document.querySelector("#pow").addEventListener("click", powq);
+document.querySelector("#equal").addEventListener("click", equalq);
+document.querySelector("#clear").addEventListener("click", clearq);
+document.querySelector("#square").addEventListener("click", squareq);
+document.querySelector("#del").addEventListener("click", delq);
+document.querySelector("#point").addEventListener("click", pointq);
+// keyboard support
+document.addEventListener("keydown", (e) => {
+    let key = e.key;
+    if (key == "+") {
+        addq();
+    } else if (key == "-") {
+        subtq();
+    } else if (key == "x") {
+        multq();
+    } else if (key == "/") {
+        divideq();
+    } else if (key == "=") {
+        equalq();
+    } else if (key == "c") {
+        clearq();
+    } else if (key == "s") {
+        squareq();
+    }
+})
+function validate(evt) {
+    var theEvent = evt || window.event;
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
     } else {
-        firstNum = display.value;
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
     }
-    display.value = "";
-    operator = "+";
-})
-document.querySelector("#subt").addEventListener("click", () => {
-    if (firstNum !== "") {
-        let res = "";
-        if (operator == "+") {
-            res = add(firstNum, display.value);
-        } else if (operator == "-") {
-            res = subtract(firstNum, display.value);
-        } else if (operator == "*") {
-            res = multiply(firstNum, display.value);
-        } else if (operator == "/") {
-            res = divide(firstNum, display.value);
-        } else if (operator == "**") {
-            res = pow(firstNum, display.value);
-        }
-        firstNum = res;
-    } else {
-        firstNum = display.value;
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
     }
-    operator = "-";
-    display.value = "";
-})
-document.querySelector("#mult").addEventListener("click", () => {
-    if (firstNum !== "") {
-        let res = "";
-        if (operator == "+") {
-            res = add(firstNum, display.value);
-        } else if (operator == "-") {
-            res = subtract(firstNum, display.value);
-        } else if (operator == "*") {
-            res = multiply(firstNum, display.value);
-        } else if (operator == "/") {
-            res = divide(firstNum, display.value);
-        } else if (operator == "**") {
-            res = pow(firstNum, display.value);
-        }
-        firstNum = res;
-    } else {
-        firstNum = display.value;
-    }
-    display.value = "";
-    operator = "*";
-})
-document.querySelector("#divide").addEventListener("click", () => {
-    if (firstNum !== "") {
-        let res = "";
-        if (operator == "+") {
-            res = add(firstNum, display.value);
-        } else if (operator == "-") {
-            res = subtract(firstNum, display.value);
-        } else if (operator == "*") {
-            res = multiply(firstNum, display.value);
-        } else if (operator == "/") {
-            res = divide(firstNum, display.value);
-        } else if (operator == "**") {
-            res = pow(firstNum, display.value);
-        }
-        firstNum = res;
-    } else {
-        firstNum = display.value;
-    }
-    display.value = "";
-    operator = "/";
-})
-document.querySelector("#pow").addEventListener("click", () => {
-    if (firstNum !== "") {
-        let res = "";
-        if (operator == "+") {
-            res = add(firstNum, display.value);
-        } else if (operator == "-") {
-            res = subtract(firstNum, display.value);
-        } else if (operator == "*") {
-            res = multiply(firstNum, display.value);
-        } else if (operator == "/") {
-            res = divide(firstNum, display.value);
-        } else if (operator == "**") {
-            res = pow(firstNum, display.value);
-        }
-        firstNum = res;
-    } else {
-        firstNum = display.value;
-    }
-    display.value = "";
-    operator = "**";
-})
-document.querySelector("#equal").addEventListener("click", () => {
-    secondNum = display.value;
-    display.value = operate(operator, firstNum, secondNum);
-    firstNum = "";
-    secondNum = "";
-    operator = "";
-})
-document.querySelector("#clear").addEventListener("click", () => {
-    firstNum = "";
-    operator = "";
-    secondNum = "";
-    display.value = "";
-})
-document.querySelector("#square").addEventListener("click", () => {
-    firstNum = display.value
-    display.value = sqrt(firstNum);
-    firstNum = "";
-    operator = "";
-})
-document.querySelector("#del").addEventListener("click", () => {
-    display.value = display.value.slice(0, -1);
-})
-document.querySelector("#point").addEventListener("click", () => {
-    if (!display.value.includes(".")) {
-        display.value = display.value + ".";
-    }
-})
-
+  }
+  
 /*
 console.log(pow(3,4));
 console.log(sqrt(64));
